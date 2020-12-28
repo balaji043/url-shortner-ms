67fd8de72e6c2e9d2dea1d3b11e8643a9b2b5309
balaji043 <cubanhog.royalgamma@gmail.com>
2020-12-28 21:38:22 +0530

    spring boot maven project initialized.

---
+++ src/test/java/tech/balaji/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,15 @@
+package tech.balaji.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
